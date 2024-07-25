246789915ed4ac8abdcd9592f9c4425999921a32
jsoliva <jsoliva@cemcol.com>
2024-07-25 15:00:53 -0600

    feat (signup): 44:40

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.http import HttpResponse
+from django.db import IntegrityError 
 
 # Create your views here.
 def home(request):
@@ -7,14 +11,37 @@ def home(request):
 
 
 def signup(request):
-    
     if request.method == 'GET':
         print("enviando formulario")
     else:
-        print("obteniendo datos")
-        print(request.POST)
-
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                #register user
+                user = User.objects.create_user(
+                    username=request.POST['username'], 
+                    password=request.POST["password1"]
+                )
+                user.save()
+                # return HttpResponse("User created succesfully")
+                login(request,user)
+                return redirect("tasks")
+            except IntegrityError:
+                return render(request, "signup.html", {
+                    "form": UserCreationForm,
+                    "error": "Username already exists"
+                })
+                # return HttpResponse("user already exists")
+            
+        return render(request, "signup.html", {
+                    "form": UserCreationForm,
+                    "error": "Password do not match"
+                })
+        # return HttpResponse("Password do not match")
 
     return render(request,'signup.html', {
         'form' : UserCreationForm
     })
+
+
+def tasks(request):
+    return render(request, "tasks.html")
